6082bc38b352715d161e6b3140921428b97a65ec
tokechan <fleatoke@gmail.com>
2025-05-05 13:48:09 +0900

    Add:Page Layout folder

---
+++ Layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <slot />
+    </div>
+</template>
+
+<style scopod>
+
+.container {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 2rem 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtLayout>
+        <h1>Nuxtchan</h1>
+    </NuxtLayout>
+</template>
+
+<style>
+h1 {
+    color:aquamarine;
+}
+</style>
