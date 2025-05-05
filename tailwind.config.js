2f733a5117b1460e2b69587d22782211eab933e3
tokechan <fleatoke@gmail.com>
2025-05-05 14:57:47 +0900

    Add:tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}',
+    './app.vue',
+    './nuxt.config.ts',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
